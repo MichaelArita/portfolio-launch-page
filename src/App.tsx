fcdf6f5014347adea4561a5a1287b7673287a98c
michaelarita <michael.arita01@gmail.com>
2024-09-04 23:48:17 -0700

    feat: Converted to TypeScript. Added Motion-Primitives. Working Vite sample page.

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import ViteReact from './pages/vite-react-default/index.tsx';
+import { motion } from 'framer-motion';
+import LandingPage from './pages/temporary-landing/index';
+// import Cursor from './components/ui/Cursor.jsx';
+import { useEffect, useState } from 'react';
+
+function App() {
+	// const [mouseX, setMouseX] = useState(0);
+	// const [mouseY, setMouseY] = useState(0);
+
+	// useEffect(() => {
+	//   function handleMouseMove(event) {
+	//     // console.log(`Mouse Position: ${event.clientX}, ${event.clientY}`);
+
+	//     setMouseX(event.clientX - 5);
+	//     setMouseY(event.clientY - 5);
+	//   }
+
+	//   document.addEventListener('mousemove', handleMouseMove);
+
+	//   return () => {
+	//     document.removeEventListener('mousemove', handleMouseMove);
+	//   }
+	// }, []);
+
+	return (
+		<>
+			{/* <Cursor clientX={mouseX} clientY={mouseY} /> */}
+			{/* <motion.div
+				animate={{ scale: [0, 1.3, 1] }}
+				transition={{ type: 'spring', bounce: 0.5, duration: 1 }}
+			> */}
+			<ViteReact />
+			{/* </motion.div> */}
+			{/* <LandingPage /> */}
+		</>
+	);
+}
+
+export default App;
